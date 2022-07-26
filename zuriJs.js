fbe10f934ad744348735bf448b9e1aa359f79d6a
isaiahAndrew <clydewitoutabonnie@gmail.com>
2022-07-26 14:03:02 +0100

    Create zuriJs.js
    display to screen

---
+++ zuriJs.js
@@ -0,0 +1,8 @@
+
+const dtls = [
+  "Isaiah Andrew Peter", 
+  '6"', 
+  "Nigeria"
+];
+const details = "My name is " + dtls[0] + ", I am " + dtls[1] + " and I am from " + dtls[2];
+console.write(details);
